be03169cdf421775dd4a8a28d1bfdd6d3c79697d
Szána Szabocs <hello@szanaszabolcs.com>
2017-09-10 20:24:28 +0200

    command feature

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _  = require('lodash'); 
+
 const notes = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('babe'));
+var command = process.argv[2];
+console.log('Command: ', command);
 
-var filteredArray = _.uniq(['Szabi', 1, 2, 3, 4, 'Szabi']);
-console.log(filteredArray);
-
-//var res = notes.addNote();
-//var name = notes.add(10, 12);
-
-//console.log(res);
-//console.log(name);
-
-//var user = os.userInfo();
-
-//fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+if(command === 'add'){
+    console.log('Adding new note');
+} else if(command === 'list') {
+    console.log('Linsting all notes');
+} else {
+    console.log('Command not recognized');
+}
