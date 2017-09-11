64c7a863eb8ae445859f770fd6e3db0c9665a72b
Szabolcs Szána <szana.szabolcs@gmail.com>
2017-09-11 12:17:51 +0200

    S3L14
    Debugging

--- app.js
+++ app.js
@@ -16,18 +16,24 @@ if(command === 'add'){
     var note = notes.addNote(argv.title, argv.body);
     if(note) {
         console.log('Note created');
-        console.log('--');
-        console.log(`Title: ${note.title}`);
-        console.log(`Body: ${note.body}`);
+        notes.logNote(note);
     } else {
         console.log('Note title taken');
     }
 } else if(command === 'list') {
     notes.getAll();
 } else if(command === 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if (note) {
+        console.log('Note found');
+        notes.logNote(note);
+    } else {
+        console.log('Note not found');
+    }
 }  else if(command === 'remove') {
-    notes.removeNote(argv.title);
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was remove' : 'Note not found';
+    console.log(message);
 } else {
     console.log('Command not recognized');
 }
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'Szabi'
+}
+
+person.age = 23;
+
+debugger;
+
+person.name = 'Szana';
+
+console.log(person);
